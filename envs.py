998821b2d2a34a7dff667514316a0198ecc8a8b0
Vitaliy Snurnitsin <vitaliy.snurnitsin@flant.com>
2018-10-07 13:10:51 +0300

    get external host from kubernetes service

--- envs.py
+++ envs.py
@@ -3,7 +3,7 @@ import os
 # without / at the end
 easyrsa_path = 'easyrsa'
 
-external_ip = '127.127.127.127'
+external_host = '192.0.2.42'
 external_port = '1194'
 
 management_host = '127.0.0.1'
@@ -12,8 +12,11 @@ management_port = '8989'
 if os.environ.get('EASYRSA_PATH') is not None:
     easyrsa_path = os.environ.get('EASYRSA_PATH')
 
-if os.environ.get('EXTERNAL_IP') is not None:
-    external_ip = os.environ.get('EXTERNAL_IP')
+if os.environ.get('EXTERNAL_HOST') is not None:
+    external_host = os.environ.get('EXTERNAL_IP')
+elif os.environ.get('KUBERNETES_SERVICE_HOST') is not None:
+    from kubernetes import *
+    external_host = get_service_external_host()
 
 if os.environ.get('EXTERNAL_PORT') is not None:
     external_port = os.environ.get('EXTERNAL_PORT')
